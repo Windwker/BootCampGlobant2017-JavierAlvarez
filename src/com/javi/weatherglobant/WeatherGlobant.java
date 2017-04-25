af0a3c8376a5e4229893ce08a77f9adbcedb84ad
Javier Alvarez <javierfernandoalvarez@gmail.com>
2017-04-25 10:18:49 -0300

    Se Agrega primera version de traductor y se maneja excepcion de Conexion

--- src/com/javi/weatherglobant/WeatherGlobant.java
+++ src/com/javi/weatherglobant/WeatherGlobant.java
@@ -10,7 +10,7 @@ public class WeatherGlobant {
 	public static String urlString = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22cordoba%2C%20argentina%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
 	public static void main(String[] args) throws Exception {
-		
+		try{
 		
 				
 		climaActual hoy = new climaActual();
@@ -63,7 +63,7 @@ public class WeatherGlobant {
 
 		System.out.println("Fecha Actualizacion: " + hoy.getFecha());
 		System.out.println("Temperatura: " + hoy.cambiarTemperatura(hoy.getTemperatura()));
-		System.out.println(hoy.getTexto());
+		System.out.println(traductor.traduceCondiciones(hoy.getTexto()));
 
 		System.out.println("\n*** CLIMA PROXIMOS DIAS ****\n");
 		ArrayList<JSONObject> listaForecast = new ArrayList<JSONObject>();
@@ -81,10 +81,14 @@ public class WeatherGlobant {
 					"Fecha: " + listaForecast.get(i).getString("day") + " " + listaForecast.get(i).getString("date"));
 			System.out.println("Temp. Minima: " + hoy.cambiarTemperatura(listaForecast.get(i).getInt("low")));
 			System.out.println("Temp. Maxima: " + hoy.cambiarTemperatura(listaForecast.get(i).getInt("high")));
-			System.out.println("Condiciones: " + listaForecast.get(i).getString("text"));
+			System.out.println("Condiciones: " + traductor.traduceCondiciones(listaForecast.get(i).getString("text")));
 			System.out.println("**********************");
 		}
-
+		}
+		catch(Exception e){
+			System.out.println("No es posible conectar al WS. Verifique Conexion");
+		}
+		
 	}
 
 }
@@ -223,6 +227,28 @@ class traductor{
 	public traductor(){
 
 		
+	}
+	public String traduceCondiciones(String condiciones){
+		
+		if(condiciones.equals("Scattered Thunderstorms")){
+			texto="Tormentas Electricas Dispersas";
+		}
+		else if(condiciones.equals("Sunny")){
+			texto="Soleado";
+		}
+		else if(condiciones.equals("Rain")){
+			texto="Luvioso";
+		}
+		
+		else if(condiciones.equals("Cloudy")){
+			texto="Nublado";
+		}
+		
+		else {
+			texto = "Error";
+		}
+		
+		return texto;
 	}
 		
 	
