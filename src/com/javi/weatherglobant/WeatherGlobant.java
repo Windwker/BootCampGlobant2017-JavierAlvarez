fda7d50909096beff355674b49c179646fdf8558
Javier Alvarez <javierfernandoalvarez@gmail.com>
2017-04-25 09:44:26 -0300

    Version Preliminar Final

--- src/com/javi/weatherglobant/WeatherGlobant.java
+++ src/com/javi/weatherglobant/WeatherGlobant.java
@@ -1,205 +1,230 @@
 package com.javi.weatherglobant;
 
-public class WeatherGlobant {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+import org.json.JSONObject;
+
+public class WeatherGlobant {
+	public static String urlString = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22cordoba%2C%20argentina%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+	public static void main(String[] args) throws Exception {
+		
+		
+				
+		climaActual hoy = new climaActual();
+		localidad localidad = new localidad();
+		traductor traductor = new traductor();
+		float temperatura;
+
 		// TODO Auto-generated method stub
 
-		Wind viento = new Wind();
-		Atmosphere atmosfera = new Atmosphere();
-		Astronomy astronomia = new Astronomy();
-		CurrentConditions condicionesActuales = new CurrentConditions();
+		URL url = new URL(urlString);
+		Scanner lector = new Scanner(url.openStream());
+		String JSON = new String();
+
+		while (lector.hasNext()) {
+			JSON += lector.nextLine();
+
+		}
+		JSONObject objeto = new JSONObject(JSON);
+
+		localidad.setCiudad(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+				.getJSONObject("location").getString("city"));
+		localidad.setPais(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+				.getJSONObject("location").getString("country"));
+		// localidad.setRegion(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("location").getString("region"));
+
+		hoy.setSensacionTemperatura(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+				.getJSONObject("wind").getInt("chill"));
+		hoy.setDireccion(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+				.getJSONObject("wind").getString("direction"));
+		hoy.setVelocidad(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+				.getJSONObject("wind").getString("speed"));
+		hoy.setTemperatura(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+				.getJSONObject("item").getJSONObject("condition").getInt("temp"));
+		hoy.setFecha(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+				.getJSONObject("item").getString("pubDate"));
+		hoy.setTexto(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+				.getJSONObject("item").getJSONObject("condition").getString("text"));
+
+		System.out.println("CLIMA ACTUAL PARA: " + localidad.getCiudad() + ", " + localidad.getPais());
+
+		/*
+		 * System.out.println(localidad.getCiudad());
+		 * System.out.println(localidad.getPais());
+		 * System.out.println(localidad.getRegion());
+		 */
+
+		// System.out.println(hoy.getSensacionTemperatura());
+		// System.out.println(hoy.getDireccion());
+		// System.out.println(hoy.getVelocidad());
+
+		System.out.println("Fecha Actualizacion: " + hoy.getFecha());
+		System.out.println("Temperatura: " + hoy.cambiarTemperatura(hoy.getTemperatura()));
+		System.out.println(hoy.getTexto());
+
+		System.out.println("\n*** CLIMA PROXIMOS DIAS ****\n");
+		ArrayList<JSONObject> listaForecast = new ArrayList<JSONObject>();
+
+		JSONObject forecast = new JSONObject();
+		for (int i = 0; i < 5; i++) {
+			forecast = objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+					.getJSONObject("item").getJSONArray("forecast").getJSONObject(i);
+			listaForecast.add(forecast);
+		}
+
+
+		for (int i = 0; i < listaForecast.size(); i++) {
+			System.out.println(
+					"Fecha: " + listaForecast.get(i).getString("day") + " " + listaForecast.get(i).getString("date"));
+			System.out.println("Temp. Minima: " + hoy.cambiarTemperatura(listaForecast.get(i).getInt("low")));
+			System.out.println("Temp. Maxima: " + hoy.cambiarTemperatura(listaForecast.get(i).getInt("high")));
+			System.out.println("Condiciones: " + listaForecast.get(i).getString("text"));
+			System.out.println("**********************");
+		}
 
 	}
 
 }
 
-class Wind {
-	int chill;
-	int direction;
-	int speed;
-
-	public Wind() {
+class localidad {
+	public localidad() {
 
 	}
 
-	public int getChill() {
-		return chill;
+	String ciudad;
+	String pais;
+	String region;
+
+	public String getCiudad() {
+		return ciudad;
 	}
 
-	public void setChill(int chill) {
-		this.chill = chill;
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
 	}
 
-	public int getDirection() {
-		return direction;
+	public String getPais() {
+		return pais;
 	}
 
-	public void setDirection(int direction) {
-		this.direction = direction;
+	public void setPais(String pais) {
+		this.pais = pais;
 	}
 
-	public int getSpeed() {
-		return speed;
+	public String getRegion() {
+		return "Region: " + region;
 	}
 
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-
-}
-
-class Atmosphere {
-	double humidity;
-	double pressure;
-	int rising;
-	double visibility;
-
-	public Atmosphere() {
-
-	}
-
-	public double getHumidity() {
-		return humidity;
-	}
-
-	public void setHumidity(double humidity) {
-		this.humidity = humidity;
-	}
-
-	public double getPressure() {
-		return pressure;
-	}
-
-	public void setPressure(double pressure) {
-		this.pressure = pressure;
-	}
-
-	public int getRising() {
-		return rising;
-	}
-
-	public void setRising(int rising) {
-		this.rising = rising;
-	}
-
-	public double getVisibility() {
-		return visibility;
-	}
-
-	public void setVisibility(double visibility) {
-		this.visibility = visibility;
+	public void setRegion(String region) {
+		this.region = region;
 	}
 
 }
 
-class Astronomy {
-	String sunrise;
-	String sunset;
+class climaActual {
 
-	public Astronomy() {
+	public climaActual() {
 
 	}
 
-	public String getSunrise() {
-		return sunrise;
+	String texto;
+
+	String velocidad;
+	String direccion;
+	int sensacionTemperatura;
+	int temperatura;
+	String fecha;
+
+	String humedad;
+	String presion;
+	String rising;
+	String visibilidad;
+	String amanecer;
+	String anochecer;
+
+	public String getTexto() {
+		return texto;
 	}
 
-	public void setSunrise(String sunrise) {
-		this.sunrise = sunrise;
+	public void setTexto(String texto) {
+		this.texto = texto;
 	}
 
-	public String getSunset() {
-		return sunset;
+	public String getFecha() {
+		return fecha;
 	}
 
-	public void setSunset(String sunset) {
-		this.sunset = sunset;
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
 	}
+
+	public int getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(int temperatura) {
+		this.temperatura = temperatura;
+	}
+
+	public void setSensacionTemperatura(int chill) {
+		this.sensacionTemperatura = chill;
+		cambiarTemperatura();
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+
+	}
+
+	public void setVelocidad(String velocidad) {
+		this.velocidad = velocidad;
+	}
+
+	public String getSensacionTemperatura() {
+
+		return "Sensacion termica:" + sensacionTemperatura;
+	}
+
+	public String getDireccion() {
+		return "Direccion de viento: " + direccion;
+	}
+
+	public String getVelocidad() {
+		return "Velocidad del viento: " + velocidad;
+	}
+
+	public void cambiarTemperatura() {
+		sensacionTemperatura = ((sensacionTemperatura - 32) * 5) / 9;
+
+	}
+
+	public int cambiarTemperatura(int temperatura) {
+		temperatura = ((temperatura - 32) * 5) / 9;
+		return temperatura;
+	}
+}
+class clima {
+	ArrayList<JSONObject> listaForecast = new ArrayList<JSONObject>();
+
+	public clima(ArrayList<JSONObject> listaForecast) {
+		this.listaForecast = listaForecast;
+
+	}
+
+
 
 }
 
-class CurrentConditions {
-	int temp;
-	String text;
-	String date;
+class traductor{
+	private String texto;
+	public traductor(){
 
-	public CurrentConditions() {
-
+		
 	}
-
-	public int getTemp() {
-		return temp;
-	}
-
-	public void setTemp(int temp) {
-		this.temp = temp;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
+		
+	
 }
 
-class Forecast {
-	String date, day, text;
-	int high, low;
-
-	public Forecast() {
-
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getDay() {
-		return day;
-	}
-
-	public void setDay(String day) {
-		this.day = day;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public int getHigh() {
-		return high;
-	}
-
-	public void setHigh(int high) {
-		this.high = high;
-	}
-
-	public int getLow() {
-		return low;
-	}
-
-	public void setLow(int low) {
-		this.low = low;
-	}
-
-}
