87aa8d618a02d62718cf7af3937aa632b33fc927
Javier Alvarez <javierfernandoalvarez@gmail.com>
2017-04-28 15:02:05 -0300

    Se inicia correccion BETA

--- src/com/javi/weatherglobant/WeatherGlobant.java
+++ src/com/javi/weatherglobant/WeatherGlobant.java
@@ -1,10 +1,12 @@
 package com.javi.weatherglobant;
 
 public class WeatherGlobant {
-
+	private String endPoint ="";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
 		CurrentConditions condicionesActuales = new CurrentConditions();
 		Forecast forecast = new Forecast();
 	}
