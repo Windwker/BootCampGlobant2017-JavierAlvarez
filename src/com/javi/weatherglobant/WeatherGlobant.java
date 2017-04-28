7287e5d4e7f0e13bd70ef1ec925918286f0145a1
Javier Alvarez <javierfernandoalvarez@gmail.com>
2017-04-28 12:59:13 -0300

    Se corrigen los dias a mostrar, se agrega una condicion de clima [Mayormente Soleado]

--- src/com/javi/weatherglobant/WeatherGlobant.java
+++ src/com/javi/weatherglobant/WeatherGlobant.java
@@ -10,85 +10,77 @@ public class WeatherGlobant {
 	public static String urlString = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22cordoba%2C%20argentina%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
 	public static void main(String[] args) throws Exception {
-		try{
-		
-				
-		climaActual hoy = new climaActual();
-		localidad localidad = new localidad();
-		traductor traductor = new traductor();
-		float temperatura;
 
-		// TODO Auto-generated method stub
+		try {
 
-		URL url = new URL(urlString);
-		Scanner lector = new Scanner(url.openStream());
-		String JSON = new String();
+			climaActual hoy = new climaActual();
+			localidad localidad = new localidad();
+			traductor traductor = new traductor();
+//			float temperatura;
 
-		while (lector.hasNext()) {
-			JSON += lector.nextLine();
+			// TODO Auto-generated method stub
 
-		}
-		JSONObject objeto = new JSONObject(JSON);
+			URL url = new URL(urlString);
+			Scanner lector = new Scanner(url.openStream());
+			String JSON = new String();
 
-		localidad.setCiudad(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
-				.getJSONObject("location").getString("city"));
-		localidad.setPais(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
-				.getJSONObject("location").getString("country"));
-		// localidad.setRegion(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("location").getString("region"));
+			while (lector.hasNext()) {
+				JSON += lector.nextLine();
 
-		hoy.setSensacionTemperatura(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
-				.getJSONObject("wind").getInt("chill"));
-		hoy.setDireccion(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
-				.getJSONObject("wind").getString("direction"));
-		hoy.setVelocidad(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
-				.getJSONObject("wind").getString("speed"));
-		hoy.setTemperatura(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
-				.getJSONObject("item").getJSONObject("condition").getInt("temp"));
-		hoy.setFecha(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
-				.getJSONObject("item").getString("pubDate"));
-		hoy.setTexto(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
-				.getJSONObject("item").getJSONObject("condition").getString("text"));
+			}
+			JSONObject objeto = new JSONObject(JSON);
 
-		System.out.println("CLIMA ACTUAL PARA: " + localidad.getCiudad() + ", " + localidad.getPais());
+			localidad.setCiudad(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+					.getJSONObject("location").getString("city"));
+			localidad.setPais(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+					.getJSONObject("location").getString("country"));
+			// localidad.setRegion(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("location").getString("region"));
 
-		/*
-		 * System.out.println(localidad.getCiudad());
-		 * System.out.println(localidad.getPais());
-		 * System.out.println(localidad.getRegion());
-		 */
+			hoy.setSensacionTemperatura(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+					.getJSONObject("wind").getInt("chill"));
+			hoy.setDireccion(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+					.getJSONObject("wind").getString("direction"));
+			hoy.setVelocidad(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+					.getJSONObject("wind").getString("speed"));
+			hoy.setTemperatura(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+					.getJSONObject("item").getJSONObject("condition").getInt("temp"));
+			hoy.setFecha(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+					.getJSONObject("item").getString("pubDate"));
+			hoy.setTexto(objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+					.getJSONObject("item").getJSONObject("condition").getString("text"));
 
-		// System.out.println(hoy.getSensacionTemperatura());
-		// System.out.println(hoy.getDireccion());
-		// System.out.println(hoy.getVelocidad());
-
-		System.out.println("Fecha Actualizacion: " + hoy.getFecha());
-		System.out.println("Temperatura: " + hoy.cambiarTemperatura(hoy.getTemperatura()));
-		System.out.println(traductor.traduceCondiciones(hoy.getTexto()));
-
-		System.out.println("\n*** CLIMA PROXIMOS DIAS ****\n");
-		ArrayList<JSONObject> listaForecast = new ArrayList<JSONObject>();
-
-		JSONObject forecast = new JSONObject();
-		for (int i = 0; i < 5; i++) {
-			forecast = objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
-					.getJSONObject("item").getJSONArray("forecast").getJSONObject(i);
-			listaForecast.add(forecast);
-		}
+			System.out.println("CLIMA ACTUAL PARA: " + localidad.getCiudad() + ", " + localidad.getPais());
 
 
-		for (int i = 0; i < listaForecast.size(); i++) {
-			System.out.println(
-					"Fecha: " + listaForecast.get(i).getString("day") + " " + listaForecast.get(i).getString("date"));
-			System.out.println("Temp. Minima: " + hoy.cambiarTemperatura(listaForecast.get(i).getInt("low")));
-			System.out.println("Temp. Maxima: " + hoy.cambiarTemperatura(listaForecast.get(i).getInt("high")));
-			System.out.println("Condiciones: " + traductor.traduceCondiciones(listaForecast.get(i).getString("text")));
-			System.out.println("**********************");
-		}
-		}
-		catch(Exception e){
+			System.out.println("Fecha Actualizacion: " + hoy.getFecha());
+			System.out.println("Temperatura: " + hoy.cambiarTemperatura(hoy.getTemperatura())+ " *");
+			System.out.println(traductor.traduceCondiciones(hoy.getTexto()));
+//			System.out.println(hoy.getTexto());
+
+			System.out.println("\n*** CLIMA PROXIMOS DIAS ****\n");
+			ArrayList<JSONObject> listaForecast = new ArrayList<JSONObject>();
+
+			JSONObject forecast = new JSONObject();
+			for (int i = 1; i < 6; i++) {
+				forecast = objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+						.getJSONObject("item").getJSONArray("forecast").getJSONObject(i);
+				listaForecast.add(forecast);
+			}
+
+			for (int i = 0; i < listaForecast.size(); i++) {
+				System.out.println("Fecha: " + listaForecast.get(i).getString("day") + " "
+						+ listaForecast.get(i).getString("date"));
+				System.out.println("Temp. Minima: " + hoy.cambiarTemperatura(listaForecast.get(i).getInt("low"))+" *");
+				System.out.println("Temp. Maxima: " + hoy.cambiarTemperatura(listaForecast.get(i).getInt("high"))+" *");
+
+//				System.out.println(listaForecast.get(i).getString("text"));
+								System.out.println("Condiciones: " + traductor.traduceCondiciones(listaForecast.get(i).getString("text")));
+				System.out.println("**********************");
+			}
+		} catch (Exception e) {
 			System.out.println("No es posible conectar al WS. Verifique Conexion");
 		}
-		
+
 	}
 
 }
@@ -210,6 +202,7 @@ class climaActual {
 		return temperatura;
 	}
 }
+
 class clima {
 	ArrayList<JSONObject> listaForecast = new ArrayList<JSONObject>();
 
@@ -218,39 +211,41 @@ class clima {
 
 	}
 
-
-
 }
 
-class traductor{
+class traductor {
 	private String texto;
-	public traductor(){
 
-		
+	public traductor() {
+
 	}
-	public String traduceCondiciones(String condiciones){
+
+	public String traduceCondiciones(String condiciones) {
+
+		if (condiciones.equals("Scattered Thunderstorms")) {
+			texto = "Tormentas Electricas Dispersas";
+		} else if (condiciones.equals("Sunny")) {
+			texto = "Soleado";
+		} else if (condiciones.equals("Rain")) {
+			texto = "Luvioso";
+		}
+
+		else if (condiciones.equals("Cloudy")) {
+			texto = "Nublado";
+		}
 		
-		if(condiciones.equals("Scattered Thunderstorms")){
-			texto="Tormentas Electricas Dispersas";
+		else if(condiciones.equals("Showers")){
+			texto = "Chaparrones";
 		}
-		else if(condiciones.equals("Sunny")){
-			texto="Soleado";
+		else if(condiciones.equals("Mostly Sunny")){
+			texto = "Mayormente Soleado";
 		}
-		else if(condiciones.equals("Rain")){
-			texto="Luvioso";
-		}
-		
-		else if(condiciones.equals("Cloudy")){
-			texto="Nublado";
-		}
-		
+
 		else {
-			texto = "Error";
+			texto = "Estado no contemplado";
 		}
-		
+
 		return texto;
 	}
-		
-	
-}
 
+}
