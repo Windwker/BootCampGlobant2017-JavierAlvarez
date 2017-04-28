a482cea5bbc02e6437895b383eff39bfdfbdac4b
Javier Alvarez <javierfernandoalvarez@gmail.com>
2017-04-28 15:07:47 -0300

    Correccion Beta

--- src/com/javi/weatherglobant/WeatherGlobant.java
+++ src/com/javi/weatherglobant/WeatherGlobant.java
@@ -1,10 +1,14 @@
 package com.javi.weatherglobant;
 
+import java.net.URL;
+
 public class WeatherGlobant {
-	private String endPoint ="";
+	private String endPoint ="https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22cordoba%2C%20argentina%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		URL url = new URL(endPoint);
 		
 		
 		CurrentConditions condicionesActuales = new CurrentConditions();
