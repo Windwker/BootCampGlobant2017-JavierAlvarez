2fe838b2b9ce1eb32ba20a0f9315b1058cd3152a
Javier Alvarez <javierfernandoalvarez@gmail.com>
2017-05-02 10:19:54 -0300

    Se agrega modulo de Forecast. Se agrega traductor y convertidor de F a C. Se corrigen metodos y llamadas de clases

--- src/com/javi/weatherglobant/WeatherGlobant.java
+++ src/com/javi/weatherglobant/WeatherGlobant.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class WeatherGlobant {
@@ -22,8 +23,13 @@ public class WeatherGlobant {
 
 		JSONObject objeto = new JSONObject(JSON);
 		CurrentConditions condicionesActuales = new CurrentConditions(
+
+				objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel").getJSONObject("item")
+						.getJSONObject("condition").getString("text"),
+				(float) objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+						.getJSONObject("item").getJSONObject("condition").getDouble("temp"),
 				new Wind(
-						objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
+						(float) objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
 								.getJSONObject("wind").getDouble("chill"),
 						objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
 								.getJSONObject("wind").getInt("direction"),
@@ -40,37 +46,37 @@ public class WeatherGlobant {
 						objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
 								.getJSONObject("atmosphere").getDouble("pressure"),
 						objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
-								.getJSONObject("atmosphere").getInt("rising"),
-						objeto.getJSONObject("query").getJSONObject("results").getJSONObject("channel")
 								.getJSONObject("atmosphere").getDouble("visibility")));
-		
-		Forecast forecast = new Forecast();
-		
+
+		Forecast forecast = new Forecast(((objeto.getJSONObject("query").getJSONObject("results")
+				.getJSONObject("channel").getJSONObject("item").getJSONArray("forecast"))));
+
 		System.out.println("***CONDICIONES ACTUALES***\n");
 		condicionesActuales.mostrarCondicionesActuales();
-		
+		System.out.println("\n***CLIMA PROXIMOS DIAS:***\n");
+		forecast.leerForecast();
 
 	}
 
 }
 
 class Wind {
-	private double chill;
+	private float chill;
 	private int direction;
 	private int speed;
 
-	public Wind(double chill, int direction, int speed) {
+	public Wind(float chill, int direction, int speed) {
 		this.chill = chill;
 		this.direction = direction;
 		this.speed = speed;
 
 	}
 
-	public double getChill() {
+	public float getChill() {
 		return chill;
 	}
 
-	public void setChill(double chill) {
+	public void setChill(float chill) {
 		this.chill = chill;
 	}
 
@@ -95,13 +101,12 @@ class Wind {
 class Atmosphere {
 	private double humidity;
 	private double pressure;
-	private int rising;
 	private double visibility;
 
-	public Atmosphere(double humidity, double pressure, int rising, double visibility) {
+	public Atmosphere(double humidity, double pressure, double visibility) {
 		this.humidity = humidity;
 		this.pressure = pressure;
-		this.rising = rising;
+
 		this.visibility = visibility;
 
 	}
@@ -120,14 +125,6 @@ class Atmosphere {
 
 	public void setPressure(double pressure) {
 		this.pressure = pressure;
-	}
-
-	public int getRising() {
-		return rising;
-	}
-
-	public void setRising(int rising) {
-		this.rising = rising;
 	}
 
 	public double getVisibility() {
@@ -172,21 +169,23 @@ class CurrentConditions {
 	private Wind viento;
 	private Astronomy astronomia;
 	private Atmosphere atmosfera;
-	private double temp;
+	private float temp;
 	private String text;
 	private String date;
 
-	public CurrentConditions(Wind viento, Astronomy astronomia, Atmosphere atmosfera) {
+	public CurrentConditions(String text, float temp, Wind viento, Astronomy astronomia, Atmosphere atmosfera) {
+		this.text = text;
+		this.temp = temp;
 		this.viento = viento;
 		this.astronomia = astronomia;
 		this.atmosfera = atmosfera;
 	}
 
-	public double getTemp() {
+	public float getTemp() {
 		return temp;
 	}
 
-	public void setTemp(double temp) {
+	public void setTemp(float temp) {
 		this.temp = temp;
 	}
 
@@ -206,8 +205,16 @@ class CurrentConditions {
 		this.date = date;
 	}
 
+	private void mostrarText() {
+		System.out.println(Auxiliares.traduceCondiciones(text));
+	}
+
+	private void mostrarTemp() {
+		System.out.println("Temperatura: " + Auxiliares.cambiarTemperatura(temp));
+	}
+
 	private void mostrarViento() {
-		System.out.println("Chill de viento: " + viento.getChill());
+		System.out.println("Sensacion Termica: " + Auxiliares.cambiarTemperatura(viento.getChill()));
 		System.out.println("Direccion de viento: " + viento.getDirection());
 		System.out.println("Velocidad de viento: " + viento.getSpeed());
 	}
@@ -215,7 +222,6 @@ class CurrentConditions {
 	private void mostrarAtmosfera() {
 		System.out.println("Humedad: " + atmosfera.getHumidity());
 		System.out.println("Presion Atmosferica: " + atmosfera.getPressure());
-		System.out.println("Rising: " + atmosfera.getRising());
 		System.out.println("Visibilidad: " + atmosfera.getVisibility());
 	}
 
@@ -223,8 +229,10 @@ class CurrentConditions {
 		System.out.println("Salida del sol: " + astronomia.getSunrise());
 		System.out.println("Puesta del sol: " + astronomia.getSunset());
 	}
-	
-	public void mostrarCondicionesActuales(){
+
+	public void mostrarCondicionesActuales() {
+		mostrarText();
+		mostrarTemp();
 		mostrarViento();
 		mostrarAtmosfera();
 		mostrarAstronomia();
@@ -235,8 +243,24 @@ class CurrentConditions {
 class Forecast {
 	private String date, day, text;
 	private double high, low;
+	JSONArray forecast;
 
-	public Forecast() {
+	public Forecast(JSONArray forecast) {
+		this.forecast = forecast;
+	}
+
+	public void leerForecast() {
+
+		for (int i = 1; i < 6; i++) {
+			System.out.println("\n" + forecast.getJSONObject(i).getString("day") + " "
+					+ forecast.getJSONObject(i).getString("date"));
+			System.out.println(
+					"Minima: " + Auxiliares.cambiarTemperatura((float) forecast.getJSONObject(i).getDouble("low")));
+			System.out.println(
+					"Maxima: " + Auxiliares.cambiarTemperatura((float) forecast.getJSONObject(i).getDouble("high")));
+			System.out.println("Estado: " + Auxiliares.traduceCondiciones(forecast.getJSONObject(i).getString("text")));
+
+		}
 
 	}
 
@@ -278,6 +302,54 @@ class Forecast {
 
 	public void setLow(double low) {
 		this.low = low;
+	}
+
+}
+
+class Auxiliares {
+	static String text;
+
+	public static float cambiarTemperatura(float temperatura) {
+		temperatura = ((temperatura - 32) * 5) / 9;
+
+		return Math.round(temperatura);
+	}
+
+	public static String traduceCondiciones(String condiciones) {
+
+		if (condiciones.equals("ScatteredThunderstorms")) {
+			text = "Tormentas Electricas Dispersas";
+		} else if (condiciones.equals("Sunny")) {
+			text = "Soleado";
+		} else if (condiciones.equals("Rain")) {
+			text = "Luvioso";
+		}
+
+		else if (condiciones.equals("Cloudy")) {
+			text = "Nublado";
+		}
+
+		else if (condiciones.equals("Showers")) {
+			text = "Chaparrones";
+		} else if (condiciones.equals("MostlySunny")) {
+			text = "Mayormente Soleado";
+		}
+
+		else if (condiciones.equals("Thunderstorms")) {
+			text = "Tormentas Electricas";
+		}
+
+		else if (condiciones.equals("PartlyCloudy")) {
+			text = "Parcialmente Nublado";
+		} else if (condiciones.equals("MostlyCloudy")) {
+			text = "Mayormente Nublado";
+		}
+
+		else {
+			text = condiciones;
+		}
+
+		return text;
 	}
 
 }
